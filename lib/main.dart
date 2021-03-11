2df120e0b2551442d76ae2b828de38a35355f301
Nbiba Bedis <bedisnbiba@gmail.com>
2021-03-11 09:29:29 +0100

    use globlas for cleaner code

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ import 'dart:isolate';
 import 'package:ffi/ffi.dart';
 import 'dart:async';
 
+////////////////
+///////FFI//////
+////////////////
+
 class MessageFFi extends Struct {
   Pointer<Void> _phantomData;
 }
@@ -15,35 +19,31 @@ typedef poll_recv_msg_type = Pointer<Utf8> Function(MessageFFi message);
 typedef send_msg_native = Void Function(MessageFFi message, Pointer<Utf8> msg);
 typedef send_msg_dart = void Function(MessageFFi message, Pointer<Utf8> msg);
 
+final path = './librust_lib.so';
+// android
+//final path = 'librust_andro_lib.so';
+final dylib = DynamicLibrary.open(path);
+final message_init = dylib
+    .lookup<NativeFunction<message_init_type>>('message_init')
+    .asFunction<message_init_type>();
+final send_msg = dylib
+    .lookup<NativeFunction<send_msg_native>>('send_msg')
+    .asFunction<send_msg_dart>();
+final poll_recv_msg = dylib
+    .lookup<NativeFunction<poll_recv_msg_type>>('poll_recv_msg')
+    .asFunction<poll_recv_msg_type>();
+
+final message_ffi = message_init("User1".toNativeUtf8());
+
+///////////////
+///////////////
+///////////////
+
 void main() {
-  var path = './librust_lib.so';
-  if (Platform.isAndroid) path = 'librust_andro_lib.so';
-  final dylib = DynamicLibrary.open(path);
-
-  final message_init = dylib
-      .lookup<NativeFunction<message_init_type>>('message_init')
-      .asFunction<message_init_type>();
-
-  final send_msg = dylib
-      .lookup<NativeFunction<send_msg_native>>('send_msg')
-      .asFunction<send_msg_dart>();
-
-  final poll_recv_msg = dylib
-      .lookup<NativeFunction<poll_recv_msg_type>>('poll_recv_msg')
-      .asFunction<poll_recv_msg_type>();
-
-  var message_ffi = message_init("User1".toNativeUtf8());
-
-  runApp(MyApp(message_ffi, send_msg, poll_recv_msg));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final MessageFFi message_ffi;
-  final send_msg_dart send_msg;
-  final poll_recv_msg_type poll_recv_msg;
-
-  MyApp(this.message_ffi, this.send_msg, this.poll_recv_msg);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -52,41 +52,28 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text("Chat"),
           ),
-          body: Body(this.message_ffi, this.send_msg, this.poll_recv_msg),
+          body: Body(),
         ));
   }
 }
 
 class Body extends StatefulWidget {
-  final MessageFFi message_ffi;
-  final send_msg_dart send_msg;
-  final poll_recv_msg_type poll_recv_msg;
-
-  Body(this.message_ffi, this.send_msg, this.poll_recv_msg);
-
   @override
-  _BodyState createState() =>
-      _BodyState(this.message_ffi, this.send_msg, this.poll_recv_msg);
+  _BodyState createState() => _BodyState();
 }
 
 class _BodyState extends State<Body> {
-  final MessageFFi message_ffi;
-  final send_msg_dart send_msg;
-  final poll_recv_msg_type poll_recv_msg;
-
   final input_control = new TextEditingController();
   var chats = [];
-
-  _BodyState(this.message_ffi, this.send_msg, this.poll_recv_msg);
 
   @override
   void initState() {
     super.initState();
     new Timer.periodic(Duration(seconds: 1), (Timer t) {
-      final msg = this.poll_recv_msg(this.message_ffi).toDartString();
+      final msg = poll_recv_msg(message_ffi).toDartString();
       if (msg != "") {
         setState(() {
-          chats.add([1, msg]);
+          this.chats.add([1, msg]);
         });
       }
     });
@@ -109,7 +96,7 @@ class _BodyState extends State<Body> {
             setState(() {
               this.chats.add([0, msg]);
             });
-            this.send_msg(this.message_ffi, msg.toNativeUtf8());
+            send_msg(message_ffi, msg.toNativeUtf8());
             this.input_control.clear();
           },
           controller: input_control),
